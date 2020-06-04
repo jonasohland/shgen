a785e2f83db879031b0eaa782a363abfc93b6582
Jonas Ohland <jonas.ohland@gmail.com>
2020-06-04 04:31:47 +0200

    fix benchmark build

--- benchmark/benchmark.cc
+++ benchmark/benchmark.cc
@@ -1,15 +1,10 @@
 #include <benchmark/benchmark.h>
 #include <iostream>
 
-#include "sheval.h"
-#include "sheval.cc"
-
 int main()
 {
     float iemdata[16];
     float shgendata[16];
-    
-    shgen::eval(2, 1.f, 1.f, 1.f, shgendata);
     
     for(int i = 0; i < 16; ++i)
         std::cout << shgendata[i] << " ";
